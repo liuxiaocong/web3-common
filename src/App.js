bd63df232fde68b44388c37fd35bcd52e0ae5219
Evan Liu <evan.liu@shopee.com>
2022-08-25 11:15:20 +0800

    get info from metamask

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
 function App() {
+  const [chainId, setChainId] = React.useState("")
+  const [account, setAccount] = React.useState("")
+  const [netVersion, setNetVersion] = React.useState("")
+  const method = {
+    getAccount: "eth_requestAccounts",
+    getChainId: "eth_chainId",
+    getNetVersion: "net_version"
+  };
+  const getInfo = async () => {
+    const { ethereum } = window;
+    if (ethereum && ethereum.isMetaMask) {
+      try {
+        const accounts = await ethereum.request({ method: method.getAccount });
+        const cid = await ethereum.request({ method: method.getChainId });
+        const nv = await ethereum.request({ method: method.getNetVersion });
+        setAccount(accounts?.[0])
+        setChainId(cid);
+        setNetVersion(nv)
+        
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.error("please install MetaMask");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Info from metamask</h1>
+        <button onClick={getInfo}>Get Info From MetaMask</button>
+        <h2>Info</h2>
+        <p>ChainId: {chainId}</p>
+        <p>Account: {account}</p>
+        <p>NetVersion: {netVersion}</p>
       </header>
     </div>
   );
